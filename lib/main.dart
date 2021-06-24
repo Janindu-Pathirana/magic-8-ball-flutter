ceac7e098dfc4a5a4d5b68f18c8228050fc9c1c3
Janindu-Pathirana <78893460+Janindu-Pathirana@users.noreply.github.com>
2021-06-24 19:51:32 +0530

    my edit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,50 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() => runApp(Myapp());
+
+class Myapp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue[100],
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: Text(
+            "Ask me anything",
+          ),
+        ),
+        body: ball(),
       ),
     );
+  }
+}
+
+var pic = 1;
+
+void changePic() {
+  pic = Random().nextInt(5) + 1;
+}
+
+class ball extends StatefulWidget {
+  @override
+  _ballState createState() => _ballState();
+}
+
+class _ballState extends State<ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        onPressed: () {
+          setState(() {
+            changePic();
+          });
+        },
+        child: Image.asset("images/ball$pic.png"),
+      ),
+    );
+  }
+}
